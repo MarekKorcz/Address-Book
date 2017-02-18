d7715d17dc78f599f2fa87d9b2af8926ac45142b
Marek Korcz <mark.korcz@gmail.com>
2017-02-18 21:39:12 +0100

    Form class and access control correction

--- src/AddressBookBundle/Form/PersonType.php
+++ src/AddressBookBundle/Form/PersonType.php
@@ -9,21 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-use Symfony\Component\OptionsResolver\OptionsResolver;
-
 class PersonType extends AbstractType{
-    
-    public function configureOptions(OptionsResolver $resolver){
-        
-        $resolver->setDefaults(
-            [
-                'csrf_protection' => true,
-                'csrf_field_name' => '_token',
-                // a unique key to help generate the secret token
-                'csrf_token_id'   => 'task_item',
-            ]
-        );
-    }
     
     public function buildForm(FormBuilderInterface $builder, array $options) {
         
@@ -32,7 +18,6 @@ class PersonType extends AbstractType{
             ->add('surname', TextType::class)
             ->add('description', TextareaType::class)
             ->add('save', SubmitType::class)
-            ->getPropertyPath('addressbook_person_createnewperson')
         ;
     }
     
--- src/AddressBookBundle/Form/PhoneType.php
+++ src/AddressBookBundle/Form/PhoneType.php
@@ -8,27 +8,13 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-use Symfony\Component\OptionsResolver\OptionsResolver;
-
 class PhoneType extends AbstractType{
-    
-    public function configureOptions(OptionsResolver $resolver) {
-        
-        $resolver->setDefaults(
-            [
-                'csrf_protection' => true,
-                'csrf_field_name' => '_token',
-                'csrf_token_id' => 'task_item'
-            ]
-        );
-    }
     
     public function buildForm(FormBuilderInterface $builder, array $options){
         
         $builder
             ->add('number', NumberType::class)
             ->add('save', SubmitType::class)
-            ->getPropertyPath('addressbook_phone_createnewphone')
         ;
     }
     
