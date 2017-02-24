d0107c3dd59077bc9fe97c1a5b2fae8114e801ed
Marek Korcz <mark.korcz@gmail.com>
2017-02-24 11:01:05 +0100

    Filling the form file

--- src/AddressBookBundle/Form/EmailType.php
+++ src/AddressBookBundle/Form/EmailType.php
@@ -5,12 +5,17 @@ namespace AddressBookBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class EmailType extends AbstractType{
     
     public function buildForm(FormBuilderInterface $builder, array $options) {
         
-        
+        $builder
+            ->add('email', EmailType::class)
+            ->add('save', SubmitType::class)
+        ;
     }
 
     public function getBlockPrefix(){
